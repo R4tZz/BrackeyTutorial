dd17e98079f1f4188273cc6dece3ff1fd5cec386
R4tZz <goncalo.moura@live.com.pt>
2024-05-04 23:26:13 +0200

    finished tutorial

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -1,9 +1,14 @@
 extends Area2D
 
 @onready var timer = $Timer
+@onready var hurt = $Hurt
 
 func _on_body_entered(body):
+	Engine.time_scale = 0.5
+	hurt.play()
+	body.get_node("CollisionShape2D").queue_free()
 	timer.start()
 	
 func _on_timer_timeout():
+	Engine.time_scale = 1.0
 	get_tree().reload_current_scene()

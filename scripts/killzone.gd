2d74e866c125900f9e4508ca43ac5ea294780aff
R4tZz <goncalo.moura@live.com.pt>
2024-05-05 13:45:37 +0200

    roll and kill enemies

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -2,12 +2,24 @@ extends Area2D
 
 @onready var timer = $Timer
 @onready var hurt = $Hurt
+@onready var slime_sprite = $"../AnimatedSprite2D"
+@onready var collision_shape_2d = $CollisionShape2D
+@onready var slime = $".."
+@onready var explosion = $Explosion
 
 func _on_body_entered(body):
-	Engine.time_scale = 0.5
-	hurt.play()
-	body.get_node("CollisionShape2D").queue_free()
-	timer.start()
+	print(body.get_node("AnimatedSprite2D").get_animation() == "Roll")
+	if body.get_node("AnimatedSprite2D").get_animation() != "Roll":
+		Engine.time_scale = 0.5
+		hurt.play()
+		body.get_node("CollisionShape2D").queue_free()
+		timer.start()
+	else:
+		slime_sprite.play("dead")
+		explosion.play()
+		collision_shape_2d.queue_free()
+		await slime_sprite.animation_finished
+		slime.queue_free()
 	
 func _on_timer_timeout():
 	Engine.time_scale = 1.0
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,17 +1,37 @@
 extends CharacterBody2D
 
-
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
+
+
+enum state {IDLE, RUN, ROLL, JUMP, FALL}
+var player_state = state.IDLE
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var animated_sprite = $AnimatedSprite2D
 @onready var jump = $Jump
 
+func update_animation():
+	if velocity.x < 0:
+		animated_sprite.flip_h = true
+	elif velocity.x > 0:
+		animated_sprite.flip_h = false
+	match(player_state):
+		state.IDLE:
+			animated_sprite.play("Idle")
+		state.RUN:
+			animated_sprite.play("Run")
+			await animated_sprite.animation_finished
+			player_state = state.IDLE
+		state.JUMP:
+			animated_sprite.play("Jump")
+		state.ROLL:
+			animated_sprite.play("Roll")
 
 func _physics_process(delta):
 	# Add the gravity.
+	
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
@@ -19,22 +39,22 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		jump.play()
-
+		#player_state = state.JUMP
+		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("move_left", "move_right")
-	if direction > 0:
-		animated_sprite.flip_h = false
-	elif direction < 0:
-		animated_sprite.flip_h = true
 	
 	if is_on_floor():
-		if direction == 0:
-			animated_sprite.play("Idle")
-		else:
-			animated_sprite.play("Run")
+		if direction == 0 and player_state != state.ROLL:
+			player_state = state.IDLE
+		elif Input.is_action_just_pressed("roll") and direction != 0:
+			player_state = state.ROLL
+		elif direction != 0 and player_state != state.ROLL:
+			player_state = state.RUN
 	else:
-		animated_sprite.play("Jump")
+		#animated_sprite.play("Jump")
+		player_state = state.JUMP
 	
 	if direction:
 		velocity.x = direction * SPEED
@@ -42,3 +62,5 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	update_animation()
+

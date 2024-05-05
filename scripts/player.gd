01af1441e77f02e3b97f3028d52de64da6c03c51
R4tZz <goncalo.moura@live.com.pt>
2024-05-05 16:43:08 +0200

    fix coyote time

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,11 +2,12 @@ extends CharacterBody2D
 
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
-const COYOTE_TIME = 0.1
+const COYOTE_TIME = 0.5
 
 enum state {IDLE, RUN, ROLL, JUMP, FALL}
 var player_state = state.IDLE
-var number_jump = 1
+var number_jump = 0
+var jump_max = 2
 var jump_available = false
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -38,18 +39,11 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	if Input.is_action_just_pressed("jump") and number_jump > 0 and jump_available:
-		velocity.y = JUMP_VELOCITY
-		jump.play()
-		number_jump = number_jump - 1
-		if number_jump == 0:
-			jump_available = false
-		
 	var direction = Input.get_axis("move_left", "move_right")
 	
 	if is_on_floor():
 		coyote_timer.stop()
-		number_jump = 1
+		number_jump = 0
 		jump_available = true
 		if direction == 0 and player_state != state.ROLL:
 			player_state = state.IDLE
@@ -58,13 +52,22 @@ func _physics_process(delta):
 		elif direction != 0 and player_state != state.ROLL:
 			player_state = state.RUN
 	else:
+		if number_jump >= jump_max and jump_available:
+			jump_available = false
 		if velocity.y < 0:
 			player_state = state.JUMP
 		else:
+			number_jump = 1
 			player_state = state.FALL
 			if jump_available:
 				if coyote_timer.is_stopped():
 					coyote_timer.start(COYOTE_TIME)
+	
+	if number_jump < jump_max:
+		if Input.is_action_just_pressed("jump") and jump_available:
+			velocity.y = JUMP_VELOCITY
+			jump.play()
+			number_jump += 1
 	
 	if direction:
 		velocity.x = direction * SPEED
